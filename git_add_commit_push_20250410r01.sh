7bdaafb23fef0fb606264f12e58c245af18a2a7a
Hakuto09 <bis2023a@gmail.com>
2025-04-11 00:31:38 +0900

    20250410r01-005_aws-cfn-stack-create

---
+++ git_add_commit_push_20250410r01.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -x
+
+if [ $# -ne 2 ];then
+#	echo "sh <branch> <version> <commit-comment>"
+	echo "sh <branch> <commit-comment>"
+#	echo "Example: sh main V1.0.0 20250311r01-001_build-test"
+	echo "Example: sh main 20250410r01-001_initial-build"
+	echo "branch list: main"
+
+	exit 1
+fi
+
+BRANCH=$1
+#echo "BRANCH=$BRANCH" > branch.txt
+echo "BRANCH=$BRANCH"
+
+template_main=meeq-device-onboarding-inet-main.yaml
+echo "main_yaml=$main_yaml"
+
+#VERSION=$2
+version=$(yq '.Mappings.Configuration.BaseConfiguration.CodebaseVersion' build/"$template_main")
+#echo "VERSION=$VERSION" > version.txt
+echo "VERSION=$VERSION"
+
+COMMENT=$2
+echo "COMMENT=$COMMENT"
+
+git add .
+git commit -m "$COMMENT"
+git push origin "$BRANCH"
+
+#rm -f branch.txt
+#rm -f version.txt
+
